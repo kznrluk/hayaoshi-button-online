ad8f9065a5421fe38d41a4074be440099afa7c89
kznrluk <kznr.luk@gmail.com>
2020-12-05 18:32:31 +0900

    パスワードの仕様を変更

--- Session/Passwords.js
+++ Session/Passwords.js
@@ -9,13 +9,14 @@ module.exports = class Passwords {
     createUniquePassword() {
         const pass = String(Math.floor( Math.random() * 10000)).padStart(4, '0');
         if (this.passMap.has(pass)) {
+            if (this.passMap.size > 9900) return '----';
             return this.createUniquePassword();
         }
         return pass;
     }
 
     checkPasswords() {
-        const expirationMs = 5 * 60 * 1000; // 15分
+        const expirationMs = 15 * 60 * 1000; // 15分
         const currentTimeMs = new Date().getTime();
         this.passMap.forEach(([createdTimeMs], key, map) => {
             if (currentTimeMs - createdTimeMs > expirationMs) {
@@ -33,7 +34,6 @@ module.exports = class Passwords {
     }
 
     createPassword(sessionId) {
-        this.removePassword(sessionId);
         const password = this.createUniquePassword();
         this.passMap.set(password, [new Date().getTime(), sessionId]);
         return password;
